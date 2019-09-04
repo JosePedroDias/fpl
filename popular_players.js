49ed352842042b780209c5ed6fcccae5fa17ddda
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-09-04 11:10:26 +0100

    update

--- popular_players.js
+++ popular_players.js
@@ -2,7 +2,20 @@ const { GKP, DEF, MID, FWD } = require('./fpl');
 
 const { readJson, sortNum } = require('./aux');
 
-const players = readJson('./data/players.json');
+const position = FWD;
+const numPlayers = 20;
+const maxPrice = 63;
+
+let players = readJson('./data/players.json');
+
+if (position) {
+  players = players.filter((p) => p.element_type === position);
+}
+
+if (maxPrice) {
+  players = players.filter((p) => p.now_cost === maxPrice);
+}
+
 sortNum(players, 'selected_by_percent').reverse();
-players.splice(20, 10000);
-console.log(players.map((p) => p.web_name).join('\n'));
+players.splice(numPlayers, 10000);
+console.log(players.map((p) => `${p.web_name} ${p.now_cost}`).join('\n'));
